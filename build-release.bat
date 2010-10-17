24187305bbab8c45ab9f44b8536d542e1fa9e89a
Matthew Strawbridge <matthew@philoxenic.com>
2010-10-17 17:14:10 +0100

    Initial commit to git

---
+++ build-release.bat
@@ -0,0 +1,2 @@
+:: Build a release executable
+..\..\..\Source\Snapshot\cobra.exe -compile -embed-run-time:yes -turbo:yes cards.cobra 
---
+++ build.bat
@@ -0,0 +1,2 @@
+..\..\..\Source\Snapshot\cobra.exe --keep-intermediate-files -d -er -dst cards.cobra 
+rem ..\..\..\Source\Snapshot\cobra.exe --keep-intermediate-files -t:winexe cards.cobra 
